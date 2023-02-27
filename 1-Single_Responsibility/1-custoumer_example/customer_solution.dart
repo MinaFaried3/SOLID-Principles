e15e5e34032f3a007fe8dc9df00152bacb74abe2
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-02-27 23:54:56 +0200

    Start Single Responsibility Examples

---
+++ 1-Single_Responsibility/1-custoumer_example/customer_solution.dart
@@ -0,0 +1,11 @@
+class Customer {
+  String? name;
+}
+
+class CustomerDB {
+  void StoreCustomer(String customerName) {}
+}
+
+class CustomerReportGenerator {
+  void generateCustomerReport(String customerName) {}
+}
